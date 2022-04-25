9445a852df3a5fe8ec0fc42f94ff7a87e13f350c
Vadim Barsukov <v.bars@icloud.com>
2022-04-25 09:20:58 +0300

    ver 0.1 bot config done

---
+++ exceptions.py
@@ -0,0 +1,7 @@
+class LoadEnvironmentError(Exception):
+    """Exception environment"""
+    pass
+
+class APIError(Exception):
+    """Exception request url endpoints"""
+    pass
--- homework.py
+++ homework.py
@@ -1,16 +1,22 @@
-...
+import logging
+import os
+import time
+
+import requests
+import telegram
+from dotenv import load_dotenv
+
+from exceptions import APIError, LoadEnvironmentError
 
 load_dotenv()
 
-
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
-
 
 HOMEWORK_STATUSES = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
@@ -18,66 +24,89 @@ HOMEWORK_STATUSES = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
+logging.basicConfig(
+    format='%(asctime)s %(name)s %(levelname)s %(funcName)s - %(message)s',
+    level=logging.INFO,
+)
+
 
 def send_message(bot, message):
-    ...
+    bot.send_message(TELEGRAM_CHAT_ID, message)
 
 
 def get_api_answer(current_timestamp):
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
+    try:
+        response = requests.get(ENDPOINT, headers=HEADERS, params=params)
+    except Exception:
+        raise APIError
 
-    ...
+    return response
 
 
 def check_response(response):
-
-    ...
+    pass
 
 
 def parse_status(homework):
-    homework_name = ...
-    homework_status = ...
+    homework_name = None
+    homework_status = None
 
-    ...
+    # ...
 
-    verdict = ...
+    verdict = None
 
-    ...
+    # ...
 
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_tokens():
-    ...
+    """Проверяет доступность идентификационных переменных"""
+    is_pass = True if (PRACTICUM_TOKEN and
+                       TELEGRAM_TOKEN and
+                       TELEGRAM_CHAT_ID) else False
+
+    if not is_pass:
+        id_vars = ['PRACTICUM_TOKEN', 'TELEGRAM_TOKEN', 'TELEGRAM_CHAT_ID']
+
+        [logging.critical(f'Переменная `{id_var}` не определена')
+         for id_var in id_vars if not os.getenv(id_var)]
+
+    return is_pass
 
 
 def main():
     """Основная логика работы бота."""
+    if not check_tokens():
+        raise LoadEnvironmentError('Ошибка загрузки токенов')
 
-    ...
-
+    # ...
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
-
-    ...
+    # ...
 
     while True:
         try:
-            response = ...
+            response = get_api_answer(current_timestamp)
+            homeworks = check_response(response)
+            for homework in homeworks:
+                msg = parse_status()
+            # ...
 
-            ...
-
-            current_timestamp = ...
+            current_timestamp = 0
             time.sleep(RETRY_TIME)
 
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            ...
+            # ...
             time.sleep(RETRY_TIME)
         else:
-            ...
+            message = ""
+            send_message(bot, message)
 
+# Exception on control breake
 
 if __name__ == '__main__':
     main()
